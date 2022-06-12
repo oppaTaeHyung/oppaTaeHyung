0815d1cb219895abecbdb258eeab5c031a864d71
oppaTaeHyung <105319213+oppaTaeHyung@users.noreply.github.com>
2022-06-11 22:33:11 -0400

    Create front-end.js

---
+++ front-end.js
@@ -0,0 +1,14 @@
+alert("hola");
+edad();
+function edad(){
+
+    edad = prompt("¿cuantos años tienes");
+    parseInt(edad);
+    if(edad >=18){
+        alert("puedes navegar tranquilamente por esta pagina web");
+    }else if( edad < 18 && edad>0){
+        alert("Lo sentimos; no tienes la edad suficiente para navegar por esta pagina web");
+        window.close();
+    }
+
+}
